1fda00d8b58afbc09dc1d502a89b495db6cc22f9
Josevillas <112953201+Josevillas@users.noreply.github.com>
2023-04-27 23:41:57 -0600

    Add files via upload

---
+++ PRUEBA PRACTICA DESARROLLADOR JUNIOR/Prueba #1 SQL.sql
@@ -0,0 +1,28 @@
+CREATE TABLE Habitacion (
+  Numero_de_habitacion INT,
+  Tipo_de_habitacion varchar(255),
+  Precio_de_la_habitacion INT,
+  Disponibilidad varchar(255),
+  PRIMARY KEY (Numero_de_habitacion)
+);
+
+CREATE TABLE Cliente (
+  Nombre_del_cliente varchar(255),
+  Direccion_del_cliente varchar(255),
+  No_Telefono varchar(24),
+  PRIMARY KEY (Nombre_del_cliente)
+);
+
+CREATE TABLE Reserva (
+  Numero_de_reserva IIDENTITY(1,1),
+  R_Nombre_del_cliente varchar(255),
+  R_Numero_de_habitacion INT,
+  Fecha_de_entrada DATE,
+  Fecha_de_salida DATE,
+  FOREIGN KEY (R_Numero_de_habitacion) REFERENCES Habitacion(Numero_de_habitacion),
+  FOREIGN KEY (R_Nombre_del_cliente) REFERENCES Cliente(Nombre_del_cliente),
+  PRIMARY KEY (Numero_de_reserva)
+);
+
+
+
